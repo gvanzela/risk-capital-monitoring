5996369df00932829ca22584ced0eee1f6bd0a31
gvanzela <gabriel.vanzela@hotmail.com>
2026-01-05 10:21:54 -0300

    Refactor SQL query to use base and ordenado CTEs

--- 4-transform/positions_exterior_aggregation.sql
+++ 4-transform/positions_exterior_aggregation.sql
@@ -1,38 +1,36 @@
-WITH posi AS (
+WITH base AS (
+    SELECT 
+        t.*
+    FROM TB_ENQ_POSICOES_FUNDOS_EXPOSTOS t
+    INNER JOIN (
+        SELECT 
+            CgePortfolio,
+            MAX(dt_carteira) AS Maxdt_carteira
+        FROM TB_ENQ_POSICOES_FUNDOS_EXPOSTOS
+        GROUP BY CgePortfolio
+    ) ult
+        ON t.CgePortfolio = ult.CgePortfolio
+       AND t.dt_carteira = ult.Maxdt_carteira
+),
+
+ordenado AS (
     SELECT
-        p.*,
-        m.asset_label AS asset_mapping_label,
-
-        CASE
-            WHEN m.asset_label = 'OPTION_EXCHANGE'
-                THEN 'OPTION_EXCHANGE'
-            WHEN m.asset_label IS NULL
-                 AND p.asset_class = 'FOREIGN_INVESTMENT'
-                THEN 'EXTERIOR'
-            ELSE 'OTHER'
-        END AS classification_filter,
-
+        *,
         ROW_NUMBER() OVER (
-            PARTITION BY
-                p.portfolio_id,
-                p.asset_nickname,
-                YEAR(p.as_of_date),
-                MONTH(p.as_of_date)
-            ORDER BY p.as_of_date DESC
+            PARTITION BY 
+                CgePortfolio,
+                dt_carteira,
+                Nickname,
+                CASE 
+                    WHEN nMCLASSIFICACAO = 'OTC SWAP' 
+                        THEN CONCAT(notional, '_', ValorSaldoAtivoSwap, '_', ValorSaldoPassivoSwap)
+                    ELSE ''
+                END
+            ORDER BY dt_insercao DESC
         ) AS rn
-
-    FROM fact_positions_snapshot AS p
-    LEFT JOIN dim_asset_mapping AS m
-           ON p.asset_type_id = m.asset_type_id
+    FROM base
 )
 
-SELECT
-    portfolio_id,
-    as_of_date,
-    SUM(position_value) AS total_exterior_value
-FROM posi
-WHERE rn = 1
-  AND classification_filter = 'EXTERIOR'
-GROUP BY
-    portfolio_id,
-    as_of_date;
+SELECT *
+FROM ordenado
+WHERE rn = 1;
