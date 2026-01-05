c9bf6ebf317d412e1e66587519fef769e6fa4203
gvanzela <gabriel.vanzela@hotmail.com>
2026-01-05 10:30:46 -0300

    Refactor SQL query to use CTE for latest positions

--- 4-transform/positions_latest.sql
+++ 4-transform/positions_latest.sql
@@ -1,11 +1,36 @@
-SELECT p.*
-FROM fact_positions_snapshot AS p
-INNER JOIN (
+WITH base AS (
     SELECT 
-        portfolio_id,
-        MAX(as_of_date) AS last_as_of_date
-    FROM fact_positions_snapshot
-    GROUP BY portfolio_id
-) latest
-    ON p.portfolio_id = latest.portfolio_id
-   AND p.as_of_date = latest.last_as_of_date;
+        t.*
+    FROM TB_ENQ_POSICOES_FUNDOS_EXPOSTOS t
+    INNER JOIN (
+        SELECT 
+            CgePortfolio,
+            MAX(dt_carteira) AS Maxdt_carteira
+        FROM TB_ENQ_POSICOES_FUNDOS_EXPOSTOS
+        GROUP BY CgePortfolio
+    ) ult
+        ON t.CgePortfolio = ult.CgePortfolio
+       AND t.dt_carteira = ult.Maxdt_carteira
+),
+
+ordenado AS (
+    SELECT
+        *,
+        ROW_NUMBER() OVER (
+            PARTITION BY 
+                CgePortfolio,
+                dt_carteira,
+                Nickname,
+                CASE 
+                    WHEN nMCLASSIFICACAO = 'OTC SWAP' 
+                        THEN CONCAT(notional, '_', ValorSaldoAtivoSwap, '_', ValorSaldoPassivoSwap)
+                    ELSE ''
+                END
+            ORDER BY dt_insercao DESC
+        ) AS rn
+    FROM base
+)
+
+SELECT *
+FROM ordenado
+WHERE rn = 1;
